bebd376d0871c7b734f05e33cd5983572cbbe297
Hecong Wang <wanghecong19991222@gmail.com>
2020-04-03 16:07:24 -0400

    Reduced to English subset of ConceptNet
    After experimentation, it is found that the entirety of ConceptNet is
    too large to be process by this implementation. Since language other
    than English benefits very little to the project, a subset of ConceptNet
    is processed and used instead.
    
    EnglishNet - a subset of ConceptNet where every concept is an English
    concept (concept with uri of the form "/c/en/.+")

--- knowledge/__main__.py
+++ knowledge/__main__.py
@@ -3,24 +3,12 @@ from __future__ import annotations
 import argparse
 import csv
 import gzip
-import json
 import time
-from typing import Optional, Tuple
+import json
 
 from knowledge import app
-from knowledge.conceptnet.models import (Assertion, Concept, Language,
-                                         PartOfSpeech, Relation, database)
-
-
-def parse_concept_uri(uri: str) -> Tuple[str, str, Optional[str], Optional[str]]:
-    assert uri.startswith("/c/"), "Invalid URI for Concept object"
-    uri = uri.split('/')[2:]
-
-    if len(uri) <= 3:
-        return tuple(uri + [None for _ in range(4 - len(uri))])
-    else:
-        return tuple(uri[:3] + ['/' + '/'.join(uri[3:])])
-
+from knowledge.conceptnet.models import (Assertion, Language, PartOfSpeech,
+                                         Relation, database)
 
 # command line arguments
 parser = argparse.ArgumentParser()
@@ -33,8 +21,6 @@ parser.add_argument("--part-of-speech-file", type=str,
                     help="CSV file containting the part-of-speech specification")
 parser.add_argument("--conceptnet-file", type=str,
                     help="ConceptNet assertions file (.csv.gz)")
-parser.add_argument("--commit-batch-size", type=int, default=1000000,
-                    help="The batch size of the database commit, larger size improves performance, while smaller size saves on memory")
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -60,79 +46,22 @@ if __name__ == "__main__":
 
         database.session.commit()
 
-        # setup for concept and assertion processing
-        LANGUAGES = database.session.query(Language).all()
-        LANGUAGES = {str(i.code): int(i.id) for i in LANGUAGES}
-
-        RELATIONS = database.session.query(Relation).all()
-        RELATIONS = {"/r/" + i.relation: int(i.id) for i in RELATIONS}
-
-        PART_OF_SPEECHES = database.session.query(PartOfSpeech).all()
-        PART_OF_SPEECHES = {str(i.code): int(i.id) for i in PART_OF_SPEECHES}
-        PART_OF_SPEECHES[None] = None  # special case
-
-        CONCEPTS = {}
-
-        # process concepts
-        concept_counter = 0
-
-        with gzip.open(args.conceptnet_file, "rt") as file:
-            reader = csv.reader(file, delimiter='\t')
-
-            for index, (_, relation, start, end, _) in enumerate(reader):
-                print(f"Processing line {index}", end='\r')
-
-                if start not in CONCEPTS.keys():
-                    concept_counter += 1
-
-                    lang, text, pos, suffix = parse_concept_uri(start)
-                    concept = Concept(id=concept_counter,
-                                      language_id=LANGUAGES[lang],
-                                      text=text,
-                                      part_of_speech_id=PART_OF_SPEECHES[pos],
-                                      suffix=suffix)
-
-                    database.session.add(concept)
-                    CONCEPTS[start] = concept_counter
-
-                if end not in CONCEPTS.keys() and relation != "/r/ExternalURL":
-                    concept_counter += 1
-
-                    lang, text, pos, suffix = parse_concept_uri(end)
-                    concept = Concept(id=concept_counter,
-                                      language_id=LANGUAGES[lang],
-                                      text=text,
-                                      part_of_speech_id=PART_OF_SPEECHES[pos],
-                                      suffix=suffix)
-
-                    database.session.add(concept)
-                    CONCEPTS[end] = concept_counter
-
-                if index % args.commit_batch_size == 0:
-                    database.session.commit()
-
-            print()
-
-        database.session.commit()
-
         # process assertions
         with gzip.open(args.conceptnet_file, "rt") as file:
+            start = time.time()
             reader = csv.reader(file, delimiter='\t')
 
-            for index, (_, relation, start, end, info) in enumerate(reader):
-                print(f"Processing line {index + 1}", end='\r')
+            for index, (uri, relation, _, _, info) in enumerate(reader):
+                print(f"Assertion processed: {index} "
+                      f"(elapsed time: {time.time() - start:.3f}s)", end='\r')
 
                 if relation == "/r/ExternalURL":
                     continue
 
-                assertions = Assertion(relation_id=RELATIONS[relation],
-                                       start_id=CONCEPTS[start],
-                                       end_id=CONCEPTS[end],
-                                       information=json.loads(info))
+                Assertion.create_from_uri(database.session, uri,
+                                          json.loads(info))
 
-                database.session.add(assertions)
-
-                if index % args.commit_batch_size == 0:
+                if index % 10000 == 0:
                     database.session.commit()
 
             print()
--- makefile
+++ makefile
@@ -2,6 +2,8 @@
 CONCEPTNET_TARGET = knowledge/data/conceptnet-assertions-5.7.0.csv.gz
 CONCEPTNET_SOURCE = 'https://s3.amazonaws.com/conceptnet/downloads/2019/edges/conceptnet-assertions-5.7.0.csv.gz'
 
+ENGLISHNET_TARGET = knowledge/data/englishnet-assertions-5.7.0.csv.gz
+
 KNOWLEDGE_DATABASE_TARGET = knowledge/data/knowledge-database.sqlite3
 KNOWLEDGE_DATABASE_LANGUAGE_SOURCE = knowledge/data/languages.csv
 KNOWLEDGE_DATABASE_RELATION_SOURCE = knowledge/data/relations.csv
@@ -14,17 +16,20 @@ KNOWLEDGE_DATABASE_DEPEND = knowledge/conceptnet/__init__.py knowledge/conceptne
 all: $(CONCEPTNET_TARGET) $(KNOWLEDGE_DATABASE_TARGET)
 
 clean:
-	rm -f $(CONCEPTNET_TARGET) $(KNOWLEDGE_DATABASE_TARGET)
+	rm -f $(CONCEPTNET_TARGET) $(ENGLISHNET_TARGET) $(KNOWLEDGE_DATABASE_TARGET)
 
 $(CONCEPTNET_TARGET):
 	wget -qNP $(@D) --show-progress $(CONCEPTNET_SOURCE)
 
+$(ENGLISHNET_TARGET): $(CONCEPTNET_TARGET)
+	gunzip -c $(CONCEPTNET_TARGET) | grep -P '^.+\t.+\t/c/en.+\t/c/en.+\t.+$$' | gzip > $@
+
 $(KNOWLEDGE_DATABASE_TARGET): $(KNOWLEDGE_DATABASE_LANGUAGE_SOURCE) \
 							  $(KNOWLEDGE_DATABASE_RELATION_SOURCE) \
 							  $(KNOWLEDGE_DATABASE_PART_OF_SPEECH_SOURCE) \
-							  $(CONCEPTNET_TARGET) $(KNOWLEDGE_DATABASE_DEPEND)
+							  $(ENGLISHNET_TARGET) $(KNOWLEDGE_DATABASE_DEPEND)
 	python -m knowledge \
-		--language-file $(KNOWLEDGE_DATABASE_LANGUAGE_SOURCE) \
-		--relation-file $(KNOWLEDGE_DATABASE_RELATION_SOURCE) \
-		--part-of-speech-file $(KNOWLEDGE_DATABASE_PART_OF_SPEECH_SOURCE) \
-		--conceptnet-file $(CONCEPTNET_TARGET)
+	--language-file $(KNOWLEDGE_DATABASE_LANGUAGE_SOURCE) \
+	--relation-file $(KNOWLEDGE_DATABASE_RELATION_SOURCE) \
+	--part-of-speech-file $(KNOWLEDGE_DATABASE_PART_OF_SPEECH_SOURCE) \
+	--conceptnet-file $(ENGLISHNET_TARGET)
